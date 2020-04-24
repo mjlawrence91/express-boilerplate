031ac0a41b4eb52aaa22bb751e93bfcd275f60bc
Matt Lawrence <mjlawrence91@gmail.com>
2020-04-24 20:13:01 +0100

    Add tests via Jest, npm scripts and coverage

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+import supertest from 'supertest'
+import app from '../src/app'
+
+describe('Base Express app', () => {
+  it('returns Hello World with 200', async () => {
+    const res = await supertest(app).get('/debug')
+
+    expect(res.status).toBe(200)
+    expect(res.text).toEqual('Hello World')
+  })
+})
