62ff12d1c6abdb2b3aa25d3d114baac32434c888
Matt Lawrence <mjlawrence91@gmail.com>
2020-05-02 19:16:16 +0100

    Update config and fix test

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,11 @@
-// import supertest from 'supertest'
-// import app from '../src/app'
+import supertest from 'supertest'
+import app from '../src/app'
 
 describe('Base Express app', () => {
-  // it('returns Hello World with 200', async () => {
-  //   const res = await supertest(app).get('/debug')
+  it('returns Hello World with 200', async () => {
+    const res = await supertest(app).get('/debug')
 
-  //   expect(res.status).toBe(200)
-  //   expect(res.text).toEqual('Hello World')
-  // })
-
-  it('fails the coverage test', () => {
-    expect(true).toBe(true)
+    expect(res.status).toBe(200)
+    expect(res.text).toEqual('Hello World')
   })
 })
